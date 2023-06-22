fcd7ad4775c8ff372d231a2860568fe71e7c1fd0
UJHa <ddkk94@naver.com>
2023-06-22 22:10:36 +0900

    애니메이션 일시정지 및 정지 처리 Slider, Text와 연결 작업

--- Assets/Scripts/MoveSetCharacter.cs
+++ Assets/Scripts/MoveSetCharacter.cs
@@ -1,5 +1,6 @@
 using System;
 using Animancer;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,7 @@ public class MoveSetCharacter : MonoBehaviour
     private AnimationClip _curClip = null;
     private AnimancerState _curState = null;
     public Slider _slider;
+    public TextMeshProUGUI _curStateTxt;
     private void Awake()
     {
         _animancer = GetComponent<AnimancerComponent>();
@@ -17,12 +19,50 @@ public class MoveSetCharacter : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            _curState.Time = 0f;
+        }
         if (Input.GetKeyDown(KeyCode.C))
         {
             _curState = _animancer.Play(_curClip);
             _curState.Time = 0f;
         }
-        if (null != _slider && null != _curState)
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            _curState.IsPlaying = false;
+        }
+
+        UpdateSlider();
+        UpdateText();
+    }
+
+    private void UpdateSlider()
+    {
+        if (null == _curState)
+            return;
+        if (null == _slider)
+            return;
+        if (_curState.IsPlayingAndNotEnding())
             _slider.value = _curState.NormalizedTime;
+        else
+            _curState.NormalizedTime = _slider.value;
+    }
+
+    private void UpdateText()
+    {
+        if (null == _curState)
+            return;
+        if (null == _curStateTxt)
+            return;
+        if (_curState.IsStopped)
+            _curStateTxt.text = "IsStopped";
+        else if (_curState.IsLooping)
+            _curStateTxt.text = "IsLooping";
+        else if (_curState.IsPlayingAndNotEnding())
+            _curStateTxt.text = "IsPlayingAndNotEnding";
+        else
+            _curStateTxt.text = "IsNone";
     }
 }
