c2466bf7b223f6b33e35e5486f96e0f092312216
UJHa <ddkk94@naver.com>
2023-07-19 23:51:11 +0900

    MoveSetScene을 관리하는 Main 클래스 생성 UI 관리용  UIManager  스크립트를 통해 동적 생성 처리

---
+++ Assets/Scripts/SceneMain/SceneMoveSet.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace SceneMain
+{
+    public class SceneMoveSet : MonoBehaviour
+    {
+        private Canvas _canvas = null;
+        private UIManager _uiManager = null;
+        private void Awake()
+        {
+            var canvasObj = GameObject.Find("Canvas");
+            if (null != canvasObj)
+            {
+                Debug.Log($"[testum]canvas obj name({canvasObj.name})");
+                if (canvasObj.TryGetComponent<Canvas>(out var canvas))
+                {
+                    _canvas = canvas;
+                    Debug.Log($"[testum]canvas find success!");
+                }
+            }
+
+            _uiManager = LoadUIManager();
+            if (null != _uiManager)
+            {
+                Debug.Log($"[testum]uiManager({_uiManager}) success");
+            }
+            else
+            {
+                Debug.Log($"[testum]uiManager({_uiManager}) fail");
+            }
+
+        }
+
+        private UIManager LoadUIManager()
+        {
+            var loadPrefab = Resources.Load<GameObject>("Prefabs/UIManager");
+            if (null == loadPrefab)
+            {
+                Debug.LogError($"[testum]uiManager({loadPrefab}) fail");
+                return null;
+            }
+        
+            Debug.Log($"[testum]uiManager({loadPrefab}) success");
+            var uiManagerObj = Instantiate(loadPrefab, _canvas.transform);
+            if (null == uiManagerObj)
+                return null;
+            if (false == uiManagerObj.TryGetComponent<UIManager>(out var uiMgr))
+                return null;
+        
+            return uiMgr;
+        }
+    }
+}
